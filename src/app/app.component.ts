1df350c7168fee54cdde9fccaf842e4d239fe4a6
VinayGoud <30427760+VinayGoud@users.noreply.github.com>
2017-10-09 16:23:21 +0530

    Hai

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Project';
+}
+export class AppComponent2 {
+   appTitle: string = 'Welcome';
+}
